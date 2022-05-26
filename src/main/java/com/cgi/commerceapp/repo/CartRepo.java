a3cfb76228761b0539258161dad34f2f6f59d580
drake.menard <drake.menard@cgi.com>
2022-05-26 15:38:02 -0500

    One to One mapping added

--- src/main/java/com/cgi/commerceapp/repo/CartRepo.java
+++ src/main/java/com/cgi/commerceapp/repo/CartRepo.java
@@ -12,4 +12,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface CartRepo extends JpaRepository<Cart, Product> {
     Cart findByItemNumber(Long itemNumber);
+    //
 }
