644b59f4b64a2394db36cd0389db3ba419fad21c
drake.menard <drake.menard@cgi.com>
2022-05-26 14:50:24 -0500

    One to One mapping added

--- src/main/java/com/cgi/commerceapp/model/Cart.java
+++ src/main/java/com/cgi/commerceapp/model/Cart.java
@@ -7,48 +7,45 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long itemNumber;
-    private String name;
-    private double price;
+//    private String name;
+//    private double price;
 
     @OneToOne(cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "product")
     private Product product;
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-        this.name= product.getName();
-        this.price=product.getPrice();
-    }
-
     public Cart() {
     }
 
     public Cart(Product product){
         super();
         this.product=product;
-        this.name= product.getName();
-        this.price=product.getPrice();
+//        this.name= product.getName();
+//        this.price=product.getPrice();
+    }
+    public Product getProduct() {
+        return product;
     }
 
+    public void setProduct(Product product) {
+        this.product = product;
+//        this.name= product.getName();
+//        this.price=product.getPrice();
+    }
     public Cart(Product product, Long itemNumber) {
         super();
         this.itemNumber = itemNumber;
         this.product = product;
-        this.name= product.getName();
-        this.price=product.getPrice();
+//        this.name= product.getName();
+//        this.price=product.getPrice();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public double getPrice() {
+//        return price;
+//    }
     public Long getItemNumber() {
         return itemNumber;
     }
